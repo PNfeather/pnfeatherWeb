3b7bfcd4509a2869d259c23c5b39e19e365cfc6b
PNFeather <921192114@qq.com>
2019-09-26 10:26:44 +0800

    指令快速提交代码

--- src/pages/homeContent/homeContent.vue
+++ src/pages/homeContent/homeContent.vue
@@ -1,6 +1,8 @@
 <template>
   <div name='homeContent'>
-    home
+    <div class="electromagnetism" data-text="欢迎来到羽的次元">
+      欢迎来到羽的次元
+    </div>
   </div>
 </template>
 
@@ -20,5 +22,43 @@
   };
 </script>
 <style scoped lang="less">
-  [name = 'homeContent']{}
+  @import '~@/style/mixin';
+  [name = 'homeContent']{
+    .electromagnetism { // 电磁干扰文
+      position: relative;
+      font-weight: bold;
+      font-size: 2rem;
+      &::before,
+      &::after {
+        color: #000;
+        overflow: hidden;
+        position: absolute;
+        top: 0;
+        clip: rect(0, 900px, 0, 0);
+        content: attr(data-text);
+        animation: shake 3s linear infinite alternate-reverse;
+      }
+      &::before {
+        left: -2px;
+        text-shadow: 1px 0 #0000ff;
+      }
+      &::after {
+        left: 2px;
+        text-shadow: -1px 0 #ff0000;
+        animation-duration: 2s;
+      }
+      @keyframes shake {
+        @steps: 20;
+        @s: ~`Math.round(Math.random() * 100) + 'px'`; // 嵌入js，需要配置less选项
+        .generate-columns(@steps);
+        .generate-columns(@n, @i: 1) when (@i =< @n) {
+          @percentage: round(percentage(@i * (1 / @n)));
+          @{percentage} {
+            clip: rect(@s, 9999px, @s, 0);
+          }
+          .generate-columns(@n, (@i + 1));
+        }
+      }
+    }
+  }
 </style>
