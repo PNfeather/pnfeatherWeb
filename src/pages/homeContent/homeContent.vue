23979802bf057127659ec7b14faaa7dafd2293a2
PNFeather <921192114@qq.com>
2019-10-08 11:20:38 +0800

    指令快速提交代码

--- src/pages/homeContent/homeContent.vue
+++ src/pages/homeContent/homeContent.vue
@@ -6,43 +6,98 @@
         <span v-for="(child, childIndex) in item.children" :key="childIndex" :class=[child.className] :ref="child.el"></span>
       </div>
     </div>
+    <div class="initial-keyframe">
+      <li><span>姓名<br>羽</span></li>
+      <li><span>姓别<br>男</span></li>
+      <li><span>年龄<br>{{years}}</span></li>
+    </div>
   </div>
 </template>
 
 <script type='text/babel'>
   import Typing from './method';
+  import {getServiceTime} from '@/api/common';
   export default {
     name: 'homeContent',
     data () {
       return {
         delayTime: 400,
+        years: '',
         pageTexts: [
           {
             el: 'welcome',
             className: 'electromagnetism',
             text: '欢迎来到羽的空间'
           }, {
-            className: 'name',
-            children: [
-              {
-                el: 'name-label',
-                className: 'labelText',
-                text: '姓名:'
-              }, {
-                el: 'name-value',
-                text: '羽'
-              }
-            ]
-          }, {
             className: 'nickName',
             children: [
               {
                 el: 'nickName-label',
                 className: 'labelText',
-                text: '常用网络ID:'
+                text: '常用网络ID'
               }, {
                 el: 'nickName-value',
-                text: 'PNfeather、紫夜羽'
+                text: '：PNfeather、紫夜羽'
+              }
+            ]
+          }, {
+            className: 'occupation',
+            children: [
+              {
+                el: 'occupation-label',
+                className: 'labelText',
+                text: '职业'
+              }, {
+                el: 'occupation-value',
+                text: '：前端工程师'
+              }
+            ]
+          }, {
+            className: 'school',
+            children: [
+              {
+                el: 'school-label',
+                className: 'labelText',
+                text: '毕业院校'
+              }, {
+                el: 'school-value',
+                text: '：西安科技大学'
+              }
+            ]
+          }, {
+            className: 'birthday',
+            children: [
+              {
+                el: 'birthday-label',
+                className: 'labelText',
+                text: '生日'
+              }, {
+                el: 'birthday-value',
+                text: '：1991-10-26'
+              }
+            ]
+          }, {
+            className: 'presentAddress',
+            children: [
+              {
+                el: 'presentAddress-label',
+                className: 'labelText',
+                text: '现居地'
+              }, {
+                el: 'presentAddress-value',
+                text: '：深圳市罗湖区'
+              }
+            ]
+          }, {
+            className: 'presentCondition',
+            children: [
+              {
+                el: 'presentCondition-label',
+                className: 'labelText',
+                text: '现状态'
+              }, {
+                el: 'presentCondition-value',
+                text: '：工作ing'
               }
             ]
           }
@@ -51,6 +106,7 @@
     },
     mounted () {
       this.start();
+      this.getYears();
     },
     methods: {
       start () { // 欢迎文案依次打出效果
@@ -74,17 +130,48 @@
           typing.startTyping();
         }, this.delayTime);
         this.delayTime += (text.length + 20) * 20;
+      },
+      getYears () {
+        getServiceTime().then(res => {
+          let data = res.data;
+          if (data.code == 0) {
+            const nowTime = new Date(data.data);
+            const diffYear = nowTime.getFullYear() - 1991;
+            const diffMonth = nowTime.getMonth() - 10;
+            const diffDate = nowTime.getDate() - 26;
+            if (diffMonth > 0) {
+              this.years = diffYear;
+            } else if (diffMonth < 0) {
+              this.years = diffYear - 1;
+            } else {
+              if (diffDate >= 0) {
+                this.years = diffYear;
+              } else if (diffDate < 0) {
+                this.years = diffYear - 1;
+              }
+            }
+          } else {
+            this.$message.error(data.msg);
+          }
+        }).catch((err) => {
+          this.$message.error(err);
+        });
       }
-    },
-    components: {}
+    }
   };
 </script>
 <style scoped lang="less">
   @import '~@/style/mixin';
   [name = 'homeContent']{
     .wh(100%, 100%);
-    [class*='section-']{
-      .wh(100%, 100%)
+    position: relative;
+    span{
+      font-size: .6rem;
+    }
+    .labelText{
+      text-align-last: justify;
+      display: inline-block;
+      width: 4rem;
     }
     .electromagnetism { // 电磁干扰文
       position: relative;
@@ -119,6 +206,46 @@
             clip: rect(@s, 9999px, @s, 0);
           }
           .generate-columns(@n, (@i + 1));
+        }
+      }
+    }
+    .initial-keyframe {
+      position: absolute;
+      right: 2rem;
+      top: 4rem;
+      width: 100px;
+      height: 100px;
+      li {
+        position: absolute;
+        border-radius: 100%;
+        width: 100%;
+        height: 100%;
+        text-align: center;
+        background-image: linear-gradient(45deg, #616B70, #1E2D3A);
+        font-size: 16px;
+        line-height: 50px;
+        color: #fff;
+        transform: rotate(0) translate(0, -80px);
+        animation: rotate 3s linear infinite;
+        &:nth-child(2) {
+          animation-delay: -1s;
+        }
+        &:nth-child(3) {
+          animation-delay: -2s;
+        }
+        span::after{
+          content: '';
+          width: 100%;
+          height: 1px;
+          background-color: #fff;
+          position: absolute;
+          left: 0;
+          top: 50%;
+        }
+      }
+      @keyframes rotate {
+        to {
+          transform: rotate(1turn) translate(0, -80px);
         }
       }
     }
